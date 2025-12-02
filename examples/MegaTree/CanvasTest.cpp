42b344a9ab7f682a1f95a0c09c2ca30dbe8856e9
Stephen Green <eelstretching@gmail.com>
2025-12-01 22:28:05 -0500

    Turn on the LED for Canvas test, so we know the program's running.

--- examples/MegaTree/CanvasTest.cpp
+++ examples/MegaTree/CanvasTest.cpp
@@ -1,3 +1,8 @@
+#include <ColorBars.h>
+#include <FadingBars.h>
+#include <RotatingColumns.h>
+#include <RotatingRows.h>
+#include <TimedAnimation.h>
 #include <stdlib.h>
 
 #include "Animator.h"
@@ -5,12 +10,13 @@
 #include "BarberPole.h"
 #include "Bouncer.h"
 #include "Canvas.h"
-#include "Strip.h"
-#include "TripleColor.h"
-#include "Spiral.h"
+#include "ColorCone.h"
+#include "Icicles.h"
 #include "LinesFill.h"
 #include "Marquees.h"
-#include "ColorCone.h"
+#include "Spiral.h"
+#include "Strip.h"
+#include "TripleColor.h"
 #include "colorutils.h"
 #include "hardware/clocks.h"
 #include "hardware/pio.h"
@@ -18,12 +24,6 @@
 #include "pico/stdio.h"
 #include "pico/stdlib.h"
 #include "pico/types.h"
-#include <TimedAnimation.h>
-#include "Icicles.h"
-#include <ColorBars.h>
-#include <FadingBars.h>
-#include <RotatingRows.h>
-#include <RotatingColumns.h>
 
 #define NUM_STRIPS 7
 #define START_PIN 2
@@ -32,12 +32,43 @@
 #define BRIGHTNESS 32
 #define FPS 30
 
+int pico_led_init(void) {
+#if defined(PICO_DEFAULT_LED_PIN)
+    // A device like Pico that uses a GPIO for the LED will define
+    // PICO_DEFAULT_LED_PIN so we can use normal GPIO functionality to turn the
+    // led on and off
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    return PICO_OK;
+#elif defined(CYW43_WL_GPIO_LED_PIN)
+    // For Pico W devices we need to initialise the driver etc
+    return cyw43_arch_init();
+#endif
+}
+
+// Turn the led on or off
+void pico_set_led(bool led_on) {
+#if defined(PICO_DEFAULT_LED_PIN)
+    // Just set the GPIO on or off
+    gpio_put(PICO_DEFAULT_LED_PIN, led_on);
+#elif defined(CYW43_WL_GPIO_LED_PIN)
+    // Ask the wifi "driver" to set the GPIO on or off
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
+#endif
+}
+
 int main() {
     stdio_init_all();
 
+    int rc = pico_led_init();
+
+    pico_set_led(true);
+    sleep_ms(1000);
+    pico_set_led(false);
+
     //
     // A canvas made out of strips.
-    Strip *strips[NUM_STRIPS];
+    Strip* strips[NUM_STRIPS];
     int ns = NUM_STRIPS;
     int pin = START_PIN;
     int pins[NUM_STRIPS];
@@ -45,18 +76,12 @@ int main() {
         pins[i] = pin;
         //
         // First strip is a little short at the moment.
-        strips[i] = new Strip(pin, i == 0 ? (STRIP_LEN-100) : STRIP_LEN);
+        strips[i] = new Strip(pin, i == 0 ? (STRIP_LEN - 100) : STRIP_LEN);
         strips[i]->setColorOrder(ColorOrder::ORGB);
     }
 
-    ArrayColorMap xmasColors({
-        RGB::Red,
-        RGB::Green,
-        RGB::Blue,
-        RGB::White, 
-        RGB::Yellow,
-        RGB::Orange
-    });
+    ArrayColorMap xmasColors({RGB::Red, RGB::Green, RGB::Blue, RGB::White,
+                              RGB::Yellow, RGB::Orange});
 
     ArrayColorMap dimXmasColors(xmasColors);
     dimXmasColors.setBrightness(32);
@@ -74,7 +99,7 @@ int main() {
     c.setup();
     c.setColorMap(&dimXmasColors);
 
-    for(int i = 0; i < c.getHeight(); i++ ) {
+    for (int i = 0; i < c.getHeight(); i++) {
         c.fillRow(i, i % dimXmasColors.getUsed());
     }
     c.show();
@@ -82,12 +107,12 @@ int main() {
 
     int nr = 0;
     while (true) {
-            c.rotateUp();
-            c.show();
-            sleep_ms(250);
-            nr++;
-            if(nr % 10 == 0) {
-                printf("Rotated %d times\n", nr);
-            }
+        c.rotateUp();
+        c.show();
+        sleep_ms(250);
+        nr++;
+        if (nr % 10 == 0) {
+            printf("Rotated %d times\n", nr);
+        }
     }
 }
